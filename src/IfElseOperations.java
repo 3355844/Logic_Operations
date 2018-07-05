10a63ab4e006f3f585392dc17d0c370ededc9435
Andrei Ponomarenko <3355844@gmail.com>
2018-07-05 20:32:47 +0300

    Try_Example_if_else3

--- src/IfElseOperations.java
+++ src/IfElseOperations.java
@@ -9,5 +9,15 @@ public class IfElseOperations {
             System.out.println("name: " + name +
                     ", age: " + age);
         }
+
+//      Тернарний оператор
+        String ageResult = (age <= 20 || name == "SomeName") ? "Ok" : name;
+        System.out.println(ageResult);
+
+        if(age == 20){
+            ageResult = "Ok";
+        }else {
+            ageResult = name;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         IfElseOperations ifElse = new IfElseOperations();
         ifElse.ifElseMethod("SomeName", -20);
-        ifElse.ifElseMethod("SomeName", 30);
-        ifElse.ifElseMethod("Andrii", 20);
+//        ifElse.ifElseMethod("SomeName", 30);
+//        ifElse.ifElseMethod("Andrii", 20);
     }
 }
