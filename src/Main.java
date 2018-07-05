875f242553cc64bcc65cc54b047b14903b510c00
Andrei Ponomarenko <3355844@gmail.com>
2018-07-05 19:43:22 +0300

    Try_Example_if_else

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        IfElseOperations ifElse = new IfElseOperations();
+        ifElse.ifElseMethod("Andrii", 20);
+        ifElse.ifElseMethod("SomeOne", 30);
+        ifElse.ifElseMethod(null, 20);
     }
 }
