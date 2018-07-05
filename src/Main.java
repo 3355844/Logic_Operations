84181a1cec74a14b82941670922fb6139e233ddc
Andrei Ponomarenko <3355844@gmail.com>
2018-07-05 20:56:14 +0300

    SwitchCaseExample

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ public class Main {
     public static void main(String[] args) {
         IfElseOperations ifElse = new IfElseOperations();
         ifElse.ifElseMethod("SomeName", -20);
-//        ifElse.ifElseMethod("SomeName", 30);
-//        ifElse.ifElseMethod("Andrii", 20);
+        ifElse.ifElseMethod("SomeName", 30);
+        ifElse.ifElseMethod("Andrii", 20);
+        SwitchCaseOperations switchCase = new SwitchCaseOperations();
+        switchCase.ageResult(20);
+        switchCase.ageResult(50);
     }
 }
--- src/SwitchCaseOperations.java
+++ src/SwitchCaseOperations.java
@@ -1,2 +1,20 @@
 public class SwitchCaseOperations {
+    public void ageResult(int age) {
+        switch (age) {
+            case 10:
+                System.out.println("Age is: " + age);
+                break;
+            case 20:
+                System.out.println("Age is: " + age);
+                break;
+            case 30:
+                System.out.println("Age is: " + age);
+                break;
+            case 40:
+                System.out.println("Age is: " + age);
+                break;
+            default:
+                System.out.println("Other value: "+ age);
+        }
+    }
 }
